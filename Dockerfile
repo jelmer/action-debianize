940af28c18333083dc04d2e92ec4dab40d88485e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-04-10 16:34:15 +0100

    Build sbuild.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM debian:sid
 
 RUN apt -y update && apt -y install git lintian-brush ognibuild sbuild
+RUN mkdir -p /srv/chroot && sbuild-createchroot --include=eatmydata,ccache unstable /srv/chroot/unstable-amd64-sbuild
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,22 @@ set -euo pipefail
 set -x
 
 IDENTITY="${1}"
+DEBIANIZE_DIRECTORY="${2}"
 
 # Default lintian-brush arguments
 debianize_args=(
     "--verbose"
-    "--diff"
+    "--iterate-fix"
+    "--schroot unstable-amd64-sbuild"
 )
+
+# Parse options
+if [ -n "${DEBIANIZE_DIRECTORY}" ]; then
+    debianize_args+=("--directory=${DEBIANIZE_DIRECTORY}")
+fi
+if [ "${DEBIANIZE_RECURSIVE}" == "true" ]; then
+    debianize_args+=("--recursive")
+fi
 
 # Create .config directory, required by breezy
 mkdir "${HOME}/.config/"
